51840570ded563c1a55ed1fc37179794ce87d2cc
Chris Upham <uphamc@etsu.edu>
2024-04-06 22:46:11 -0400

    CKHN-108 Chris and Noah API Gateway 4/6/24
    Created what we think is an API gateway.  We need to have the actual microservices to reference to move forward and test if it actually works.

--- GameMicroServer/Controllers/microController.cs
+++ GameMicroServer/Controllers/microController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 
 using Microsoft.Extensions.Caching.Distributed;
 using System.Text.Json;
+using GameMicroServer.Services;
 
 namespace Micro
 {
@@ -16,8 +17,9 @@ namespace Micro
     {
         private static readonly List<GameInfo> TheInfo = new List<GameInfo>
         {
+            //Remove this code once individual microservices are set up
             new GameInfo { 
-                //Id = 1,
+                Id = 1,
                 Title = "Snake",
                 //Content = "~/js/snake.js",
                 Author = "Hillary clinton ",
@@ -27,7 +29,7 @@ namespace Micro
                 //Thumbnail = "/images/snake.jpg" //640x360 resolution
             },
             new GameInfo { 
-                //Id = 2,
+                Id = 2,
                 Title = "Tetris",
                 //Content = "~/js/tetris.js",
                 Author = "Steve from minecraft",
@@ -37,7 +39,7 @@ namespace Micro
                 //Thumbnail = "/images/tetris.jpg"
             },
             new GameInfo { 
-                //Id = 3,
+                Id = 3,
                 Title = "Pong",
                 //Content = "~/js/pong.js",
                 Author = "Forest Gump",
@@ -54,6 +56,40 @@ namespace Micro
         public MicroController(ILogger<MicroController> logger)
         {
             _logger = logger;
+        }
+
+        private readonly IGameRepository _gameRepo;
+        //public MicroController(IGameRepository gameRepo)
+        //{
+        //    _gameRepo = gameRepo;
+        //}
+
+        //[HttpGet("game/{id}")]
+        //public IActionResult Get(int id)
+        //{
+        //    var game = _gameRepo.GetByIdAsync(id);
+        //    if (game == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(game);
+        //}
+        [HttpGet("Games/Play/{id}")]
+        public GameInfo GetById(int id)
+        {
+            GameInfo info = new GameInfo
+            {
+                Id = 1,
+                Title = "Tetris",
+                //Content = "~/js/tetris.js",
+                Author = "Steve from minecraft",
+                DateAdded = "09/09/1541",
+                Description = "Block Block Block",
+                HowTo = "Put Blocks Down",
+                //Thumbnail = "/images/tetris.jpg"
+            };
+            //Take the requested id and then go to that contoller and return the necessary info
+            return info; // This would be returning hte info from the microservice
         }
 
         [HttpGet]
